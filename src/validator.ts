6a78b74d5a03d26892696100ec38a3a11b98e324
NicoleHSilva <nicolehenriquesilva@gmail.com>
2026-08-27 19:24:50 -0300

    Atividade: Implementar validação de título do chamado

--- src/validator.ts
+++ src/validator.ts
@@ -4,11 +4,11 @@ function validarChamado(titulo: string): boolean {
     }
 
     if (titulo.trim().length < 5) {
-        throw new Error("O título deve possuir no mínimo 5 caracteres.");
+        throw new Error("O título do chamado deve possuir no mínimo 5 caracteres.");
     }
 
     if (titulo.trim().length > 100) {
-        throw new Error("O título deve possuir no máximo 100 caracteres.");
+        throw new Error("O título do chamado deve possuir no máximo 100 caracteres.");
     }
 
     return true;
